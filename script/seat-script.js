dde75ab0443e623bc607762f69fcc187ee1447ed
Jia Jun <damontham@hotmail.com>
2022-10-20 13:29:56 +0800

    Add ticket-script

---
+++ script/seat-script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".ticket-seating-plan-container");
+const seats = document.querySelectorAll(
+  ".ticket-seat-row .ticket-seat:not(.occupied):not(.not-available)"
+);
+const count = document.getElementById("seat-count");
+const movie = document.getElementById("movie-name");
+
+let ticketPrice = 15.0;
+
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("ticket-seat") &&
+    !e.target.classList.contains("occupied") &&
+    !e.target.classList.contains("not-available")
+  ) {
+    e.target.classList.toggle("selected");
+    // console.log(e.target.value);
+  }
+});
