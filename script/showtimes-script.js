a58e3b478bd87966384eb76636cddadebd8c07b4
Jia Jun <damontham@hotmail.com>
2022-10-25 17:38:14 +0800

    Added script to select date

---
+++ script/showtimes-script.js
@@ -0,0 +1,60 @@
+const firstDate = document.querySelector(".showtimes-date-1");
+const secondDate = document.querySelector(".showtimes-date-2");
+const thirdDate = document.querySelector(".showtimes-date-3");
+const leftArrow = document.querySelector(".showtimes-date-leftarrow");
+const rightArrow = document.querySelector(".showtimes-date-rightarrow");
+const selectedDateContainer = document.querySelector(".showtimes-date");
+const selectedDate = document.querySelectorAll(".showtimes-timing");
+var count = 0;
+const today = new Date();
+
+function calculateDate() {
+  var dateOne = new Date();
+  dateOne.setDate(dateOne.getDate() + count);
+  var dateTwo = new Date(dateOne);
+  dateTwo.setDate(dateTwo.getDate() + 1);
+  var dateThree = new Date(dateOne);
+  dateThree.setDate(dateThree.getDate() + 2);
+
+  if (dateOne < today) {
+    alert("Cannot select a date that is earlier than today");
+    count = 0;
+    dateOne.setDate(today.getDate());
+
+    dateTwo = new Date(dateOne);
+    dateTwo.setDate(dateTwo.getDate() + 1);
+
+    dateThree = new Date(dateOne);
+    dateThree.setDate(dateThree.getDate() + 2);
+  }
+  firstDate.innerHTML = dateOne.toDateString();
+  secondDate.innerHTML = dateTwo.toDateString();
+  thirdDate.innerHTML = dateThree.toDateString();
+}
+
+// initialize
+calculateDate();
+localStorage.setItem("Date", today.toDateString());
+
+leftArrow.addEventListener("click", function (e) {
+  count--;
+  calculateDate();
+});
+rightArrow.addEventListener("click", function (e) {
+  count++;
+  calculateDate();
+});
+
+selectedDateContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("showtimes-date-1") ||
+    e.target.classList.contains("showtimes-date-2") ||
+    e.target.classList.contains("showtimes-date-3")
+  ) {
+    selectedDate.forEach((date) => {
+      date.classList.remove("selected");
+    });
+    e.target.classList.toggle("selected");
+    localStorage.setItem("Date", e.target.innerHTML);
+  }
+});
