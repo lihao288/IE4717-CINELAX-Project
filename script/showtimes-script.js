772dc4f622f8e317c8da1310d654676a05ef34ed
Jia Jun <damontham@hotmail.com>
2022-10-26 14:45:06 +0800

    Update the link to buy ticket and jumps to the section where the movie at

--- script/showtimes-script.js
+++ script/showtimes-script.js
@@ -112,3 +112,25 @@ timeSelectionContainer.forEach((timeSelection) => {
     }
   });
 });
+
+// // When the user scrolls the page, execute stickyFunc
+// window.onscroll = function () {
+//   stickyFunc();
+// };
+
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // Get the offset position of the navbar
+// var sticky = dateSelectionContainer.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyFunc() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//     dateSelectionContainer.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//     dateSelectionContainer.classList.remove("sticky");
+//   }
+// }
